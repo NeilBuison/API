072f07e7647452d02df3da667256b2f86b5cd83e
Neil Patrick Buison <neilpatrickbuison@gmail.com>
2021-05-18 08:15:34 -0400

    API

---
+++ POST/Response.java
@@ -0,0 +1,6 @@
+{
+    "name": "morpheus",
+    "job": "leader",
+    "id": "350",
+    "createdAt": "2021-05-17T14:01:01.550Z"
+}
---
+++ PUT-PATCH/Response.java
@@ -0,0 +1,5 @@
+{
+    "name": "morpheus",
+    "job": "zion resident",
+    "updatedAt": "2021-05-17T14:03:22.827Z"
+}
